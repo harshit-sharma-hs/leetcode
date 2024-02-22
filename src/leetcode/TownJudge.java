f7ea4e72c01cb9a39fd751a2f447daeb1619d318
harshit-sharma-hs <harshitsharma4848@gmail.com>
2024-02-22 21:07:10 +0530

    997. Find the Town Judge | leetcode |

---
+++ src/leetcode/TownJudge.java
@@ -0,0 +1,50 @@
+/*
+ * 997. Find the Town Judge
+ */
+package leetcode;
+
+import java.util.Scanner;
+
+class TownJudge {
+	public static int findJudge(int n, int[][] trust) {
+		int m = trust.length;
+		int indegree[] = new int[n + 1];
+		for (int i = 0; i < m; i++) {
+			indegree[trust[i][1]]++;
+		}
+
+		int answer = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (indegree[i] == n - 1) {
+				answer = i;
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			if (trust[i][0] == answer) {
+				return -1;
+			}
+		}
+
+		return answer == 0 ? -1 : answer;
+
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		int[][] arr = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println(findJudge(m, arr));
+
+	}
+}
