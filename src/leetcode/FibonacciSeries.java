e4eaa770da29fe3e1824ed66370af6d79a3ea51a
harshit-sharma-hs <harshitsharma4848@gmail.com>
2024-03-23 20:42:06 +0530

    143. Reorder List | GFG | Easy

---
+++ src/leetcode/FibonacciSeries.java
@@ -0,0 +1,19 @@
+/*
+ * 143. Reorder List
+ */
+package leetcode;
+
+class FibonacciSeries {
+
+	int[] Series(int n) {
+		int arr[] = new int[n + 1];
+		arr[0] = 0;
+		arr[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
+			arr[i] %= 1000000007;
+		}
+		return arr;
+	}
+}
