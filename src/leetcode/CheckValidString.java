6f277b77d2a7695c1f5f2e5e49171fab4ac85ae8
harshit-sharma-hs <harshitsharma4848@gmail.com>
2024-04-07 20:17:25 +0530

    IMPORTANT : 678. Valid Parenthesis String | leetcode | medium | took Help

---
+++ src/leetcode/CheckValidString.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class CheckValidString {
+    public boolean checkValidString(String s) {
+        
+        int low=0;
+        int high =0;
+        for(char c:s.toCharArray())
+        {
+            low+=c=='('?1:-1;
+            high+= c!=')'?1:-1;
+            if(high<0)
+                return false;
+            low=Math.max(0,low);
+        }
+        return low==0;
+    }
+}
