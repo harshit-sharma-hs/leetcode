49a0e6a01089b1d6ac0aaade57aa0447b0523a08
harshit-sharma-hs <harshitsharma4848@gmail.com>
2024-04-09 20:31:58 +0530

    2073. Time Needed to Buy Tickets | easy | leetcode | self

---
+++ src/leetcode/TimeRequiredToBuy.java
@@ -0,0 +1,21 @@
+/*
+ * 2073. Time Needed to Buy Tickets
+ */
+package leetcode;
+
+class TimeRequiredToBuy {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        while (tickets[k] > 0) {
+            for (int i = 0; i < tickets.length; i++) {
+                if (tickets[i] > 0) {
+                    tickets[i] = tickets[i] - 1;
+                    time++;
+                }
+                if (tickets[k] == 0)
+                    break;
+            }
+        }
+        return time;
+    }
+}
